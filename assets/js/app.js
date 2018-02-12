de7db3c78553362bd9c567e1314affc8b868df6e
liannaccone <lciannaccone@gmail.com>
2018-02-11 18:33:14 -0800

    removing unused, commented out code to clean up js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,9 +24,14 @@ var database = firebase.database();
 
 // ======================================================================
 
+
+
+
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// 								DASHBOARD.HTML
+// 								INDEX.HTML
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 	// listener for on click of enter button.....
 	$(document).on("click", ".enter-btn", function() {
@@ -34,8 +39,6 @@ var database = firebase.database();
 		// brings user to dashboard.html page
 		document.location="dashboard.html"
 	});
-
-
 
 
 
@@ -297,85 +300,3 @@ database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
 
 
 
-// ========= ELEPHANT GRAVEYARD (unused, preserved code)
-//  JUST UPDATES FOR A SPECIFIC JOB (JOB-VIEW)
-
-
-// // firebase watcher & initial loader
-// database.ref().on("child_added", function(snapshot) {
-// 	// creating variable to call the output pathway
-// 	var snap = snapshot.val();
-
-// 	// populates job-view html fields with user input data from firebase
-// 	$("#date-applied").html(snap.dateApplied);
-// 	$("#app-summary").html(snap.appSummary);
-// 	$("#status").html(snap.Status);
-
-
-// 	// ajax call to populate job posting data from jobID saved in firebase
-	
-// 	// creating variable to store jobID from firebase
-// 	var jobID = snap.jobID;
-
-// 	var queryURL = "https://api-v2.themuse.com/jobs/" + jobID
-
-// 	$.ajax({
-// 		url: queryURL,
-// 		method: "GET"
-// 	}).then(function(response){
-// 		console.log(response)
-// 		$(".company-name").html(response.company.name);
-// 		$(".job-position").html(response.name);
-// 		$(".job-description").html(response.contents);
-// 		// adds attribute to map class storing the city from Muse API, to be used with google maps api
-// 		$(".map").attr("data-city", response.locations[0].name)
-
-// 		});
-
-
-// }, function(errorOnject) {
-// 	console.log("Errors handled: " + errorObject.code);
-// });
-
-
-// =============================================================
-
-  
-
-
-
-
-// ==============================
-
-
-
-
-
-
-// ------------------------------------------------
-
-	// ajax call for Adzuna
-
-		// function ajaxAdzuna () {
-
-		//  	var category = "";
-		//  	// need to fix location search to replace spaces with "%20" 
-		//  	var location = ""
-		//  	var salary_min = 50000
-		//  	var appID = "0b80554a"
-		//  	var apiKey = "472827f96df2b335cc9979fcb8db1fb5";
-		// 	var queryURL = "http://api.adzuna.com:80/v1/api/jobs/gb/search/1?app_id="+ appID + "&app_key="+apiKey + "&results_per_page=20&what=javascript%20developer&what_exclude=java&where="+ location + "&sort_by=salary&salary_min=" + salary_min + "&full_time=1&permanent=1&content-type=application/json"
-
-		//  	$.ajax({
-		//  		url: queryURL,
-		//  		method: "GET"
-		//  	}).then(function(response){
-				
-		//  		console.log(response.results[0])
-		//  		$("#job-posting").html(response.results[0].description)
-		//  		// $("#job-posting").html(response.results[0].contents)
-
-		//  	});
-		//  };
-
-// ------------------------------------------------
